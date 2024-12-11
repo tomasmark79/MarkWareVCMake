fb3a75a521c7fafc266dc39fd81be9542bd5600e
tomasmark79 <tomas@digitalspace.name>
2024-12-11 17:51:20 +0100

    doc update

--- Source/VCMLib.cpp
+++ Source/VCMLib.cpp
@@ -1,14 +1,12 @@
 #include "VCMLib/VCMLib.hpp"
 
 #include <iostream>
-#include <vcmlib/version.h>
 
 /* Start build 🔨 with CTRL + ALT + B */
 
 VCMLib::VCMLib()
 {
-    std::cout << "MarkWare VCMake Library version: " << std::endl;
-    std::cout << "Version: " << VCMLIB_VERSION << std::endl;
+    std::cout << "-- MarkWare VCMake Library --" << std::endl;
 }
 
 VCMLib::~VCMLib() {}
