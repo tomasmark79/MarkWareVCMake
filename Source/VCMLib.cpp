c91cbcbfda38753eb02ec5380f127c7da2302c1c
tomasmark79 <tomas@digitalspace.name>
2024-12-15 09:12:58 +0100

    -- MarkWare VCMake Library Linked --

--- Source/VCMLib.cpp
+++ Source/VCMLib.cpp
@@ -6,6 +6,6 @@
 // Those are the implementations of the class VCMLib
 // (c) Tomáš Mark 2024
 
-VCMLib::VCMLib() { std::cout << "-- MarkWare VCMake Library --" << std::endl; }
+VCMLib::VCMLib() { std::cout << "-- MarkWare VCMake Library Linked --" << std::endl; }
 
 VCMLib::~VCMLib() {}
