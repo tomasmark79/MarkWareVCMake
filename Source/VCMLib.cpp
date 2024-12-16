3147db073eb88b065e0f3613f77b6d6c1bfe8c61
tomasmark79 <tomas@digitalspace.name>
2024-12-16 16:49:34 +0100

    openssl implemented if enabled

--- Source/VCMLib.cpp
+++ Source/VCMLib.cpp
@@ -3,9 +3,22 @@
 #include <EmojiToolsLib/EmojiToolsLib.hpp>
 #include <iostream>
 
+#ifdef ENABLE_OPENSSL
+#    include <openssl/crypto.h>
+#    include <openssl/ssl.h>
+#endif
+
 // Those are the implementations of the class VCMLib
 // (c) Tomáš Mark 2024
 
-VCMLib::VCMLib() { std::cout << "-- MarkWare VCMake Library Linked --" << std::endl; }
+VCMLib::VCMLib()
+{
 
-VCMLib::~VCMLib() {}
+    std::cout << "-- MarkWare VCMake Library Linked --" << std::endl;
+
+#ifdef OPENSSL_VERSION
+    std::cout << OPENSSL_VERSION << std::endl;
+#endif
+}
+
+VCMLib::~VCMLib() { std::cout << "-- MarkWare VCMake Library Unlinked --" << std::endl; }
