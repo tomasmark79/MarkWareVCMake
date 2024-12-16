c4c34d60d689fed7354c153baaee529551baad71
tomasmark79 <tomas@digitalspace.name>
2024-12-16 16:49:24 +0100

    refactored, fixed issues

--- cmake/OpenSSLConfig.cmake
+++ cmake/OpenSSLConfig.cmake
@@ -8,28 +8,38 @@ if(ENABLE_OPENSSL)
 endif()
 
 if(mwopenssl_ADDED)
+    message(STATUS "OpenSSL added")
+
     set(OPENSSL_LIB_DIR
         ${mwopenssl_SOURCE_DIR}/mwopenssl-install-${CMAKE_SYSTEM_PROCESSOR}-${CMAKE_BUILD_TYPE}
     )
     message(STATUS "OPENSSL_LIB_DIR: ${OPENSSL_LIB_DIR}")
-    
+
     set(OPENSSL_INCLUDE_DIR ${OPENSSL_LIB_DIR}/include)
 
     set(OPENSSL_CRYPTO_LIBRARY ${OPENSSL_LIB_DIR}/lib/libcrypto.a)
     set(OPENSSL_SSL_LIBRARY ${OPENSSL_LIB_DIR}/lib/libssl.a)
-    set(OPENSSL_APPLINK_LIBRARY ${OPENSSL_LIB_DIR}/lib/applink.c)
 
+    message(STATUS "--------------------------------------------------------------")
     message(STATUS "mwopenssl_SOURCE_DIR: ${mwopenssl_SOURCE_DIR}")
-    message(STATUS "OPENSSL_INCLUDE_DIR: ${OPENSSL_INCLUDE_DIR}")
+    message(STATUS "mwopenssl_BINARY_DIR: ${mwopenssl_BINARY_DIR}")
     message(STATUS "OPENSSL_LIB_DIR: ${OPENSSL_LIB_DIR}")
+    message(STATUS "OPENSSL_INCLUDE_DIR: ${OPENSSL_INCLUDE_DIR}")
     message(STATUS "OPENSSL_CRYPTO_LIBRARY: ${OPENSSL_CRYPTO_LIBRARY}")
     message(STATUS "OPENSSL_SSL_LIBRARY: ${OPENSSL_SSL_LIBRARY}")
-    message(STATUS "OPENSSL_APPLINK_LIBRARY: ${OPENSSL_APPLINK_LIBRARY}")
+    message(STATUS "--------------------------------------------------------------")
 
     if(CMAKE_BUILD_TYPE STREQUAL "Debug")
         set(OPENSSL_BUILD_TYPE "--debug")
     else()
         set(OPENSSL_BUILD_TYPE "--release")
+    endif()
+
+    if(BUILD_SHARED_LIBS)
+        set(OPENSSL_SHARED_LIBS "shared")
+    else()
+        # set(OPENSSL_SHARED_LIBS "no-shared no-pinshared")
+        set(OPENSSL_SHARED_LIBS "")
     endif()
 
     if(CMAKE_CROSSCOMPILING)
@@ -38,17 +48,17 @@ if(mwopenssl_ADDED)
                 OUTPUT ${OPENSSL_CRYPTO_LIBRARY} ${OPENSSL_SSL_LIBRARY}
                 WORKING_DIRECTORY ${mwopenssl_SOURCE_DIR}
                 COMMAND
-                ./Configure linux-aarch64 ${OPENSSL_BUILD_TYPE} --libdir=${OPENSSL_LIB_DIR}/lib
-                no-docs enable-pie --prefix=${OPENSSL_LIB_DIR} --openssldir=${CMAKE_SYSROOT}
-                CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} CPP=${CMAKE_C_COMPILER}\ -E
-                CXXCPP=${CMAKE_CXX_COMPILER}\ -E
+                    ./Configure linux-aarch64 ${OPENSSL_BUILD_TYPE} ${OPENSSL_SHARED_LIBS}
+                    --libdir=${OPENSSL_LIB_DIR}/lib no-tests no-docs --prefix=${OPENSSL_LIB_DIR}
+                    --openssldir=${CMAKE_SYSROOT} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
+                    CPP=${CMAKE_C_COMPILER}\ -E CXXCPP=${CMAKE_CXX_COMPILER}\ -E
                 COMMAND ${CMAKE_MAKE_PROGRAM} clean
                 COMMAND ${CMAKE_MAKE_PROGRAM} -j$(nproc)
                 COMMAND ${CMAKE_MAKE_PROGRAM} install -j$(nproc)
                 COMMENT "Cross-building openssl"
             )
         else()
-            message(STATUS "OpenSSL libraries already exist, skipping build step")
+            message(STATUS "OpenSSL libraries already exist, skipping build step :-)")
         endif()
     else()
         if(NOT EXISTS ${OPENSSL_CRYPTO_LIBRARY} OR NOT EXISTS ${OPENSSL_SSL_LIBRARY})
@@ -56,8 +66,9 @@ if(mwopenssl_ADDED)
                 OUTPUT ${OPENSSL_CRYPTO_LIBRARY} ${OPENSSL_SSL_LIBRARY}
                 WORKING_DIRECTORY ${mwopenssl_SOURCE_DIR}
                 COMMAND
-                ./Configure linux-x86_64 ${OPENSSL_BUILD_TYPE} --libdir=${OPENSSL_LIB_DIR}/lib
-                no-docs enable-pie --prefix=${OPENSSL_LIB_DIR} --openssldir=${OPENSSL_LIB_DIR}
+                    ./Configure linux-x86_64 ${OPENSSL_BUILD_TYPE} ${OPENSSL_SHARED_LIBS}
+                    --libdir=${OPENSSL_LIB_DIR}/lib no-tests no-docs --prefix=${OPENSSL_LIB_DIR}
+                    --openssldir=${OPENSSL_LIB_DIR}
                 COMMAND ${CMAKE_MAKE_PROGRAM} clean
                 COMMAND ${CMAKE_MAKE_PROGRAM} -j$(nproc)
                 COMMAND ${CMAKE_MAKE_PROGRAM} install -j$(nproc)
@@ -68,7 +79,11 @@ if(mwopenssl_ADDED)
         endif()
     endif()
 
-    add_custom_target(build-openssl DEPENDS ${OPENSSL_CRYPTO_LIBRARY} ${OPENSSL_SSL_LIBRARY})
-    add_dependencies(${PROJECT_LIBRARY_NAME} build-openssl)
+    # create target object
+    add_custom_target(build-openssl ALL DEPENDS ${OPENSSL_SSL_LIBRARY} ${OPENSSL_CRYPTO_LIBRARY})
     target_include_directories(${PROJECT_LIBRARY_NAME} PUBLIC ${OPENSSL_INCLUDE_DIR})
+    # order is important
+    target_link_libraries(
+        ${PROJECT_LIBRARY_NAME} PUBLIC ${OPENSSL_SSL_LIBRARY} ${OPENSSL_CRYPTO_LIBRARY}
+    )
 endif()
