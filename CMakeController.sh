8e2f071d5c7a1edb0912920acb830dbac280acbb
tomasmark79 <tomas@digitalspace.name>
2024-12-17 19:35:25 +0100

    added new tasks for "Build All", "ReBuild All", "Configure All", "Conan Install All",
    we need todo to clean and refactor CMakeController.sh

--- CMakeController.sh
+++ CMakeController.sh
@@ -3,6 +3,8 @@
 # This script is a controller for CMake tasks
 # (c) Tomáš Mark 2024
 
+workSpaceDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 # Arguments and defaults
 taskName=$1
 archBuildType=$2
@@ -11,12 +13,12 @@ buildType=${3:-"Release"} # Default to "Release" if not specified
 GREEN='\033[0;32m' NC='\033[0m' YELLOW='\033[1;33m'
 echo -e "${GREEN}CMakeController.sh: args: [" $taskName $archBuildType $buildType "]${NC}"
 
-# Determine workspace and toolchain file
-workSpaceDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
 # Helper function for Conan install
 function conan_install() {
+
+    # Activate Python environment where Conan is installed
     source /home/tomas/.pyenv/versions/3.9.2/envs/env392/bin/activate
+
     local buildDir=$1
     local conanCommand="conan install $workSpaceDir --output-folder=$buildDir --build=missing --profile=default"
     echo $conanCommand
@@ -79,8 +81,61 @@ function get_build_dir() {
     echo "Build/${type}/${archBuildType}/${buildType}"
 }
 
+function ConanInstall() {
+    conan_install "$(get_build_dir Standalone)"
+    conan_install "$(get_build_dir Library)"
+}
+
+function Configure() {
+    cmake_configure "." "$(get_build_dir Library)"
+    cmake_configure "./Standalone" "$(get_build_dir Standalone)"
+}
+
+function ReBuild() {
+    ConanInstall
+    Configure
+    cmake_build "$(get_build_dir Library)"
+    cmake_build "$(get_build_dir Standalone)"
+}
+
+function Build() {
+    cmake_build "$(get_build_dir Library)"
+    cmake_build "$(get_build_dir Standalone)"
+}
+
+function Clean() {
+    cmake_clean "$(get_build_dir Library)"
+    cmake_clean "$(get_build_dir Standalone)"
+}
+
 # Task dispatcher
 case $taskName in
+
+"Conan Install All")
+    ConanInstall
+    exit 0
+    ;;
+
+"Configure All")
+    Configure
+    exit 0
+    ;;
+
+"Build All")
+    Build
+    exit 0
+    ;;
+
+"ReBuild All")
+    ReBuild
+    exit 0
+    ;;
+
+"Clean All")
+    Clean
+    exit 0
+    ;;
+
 "Conan Install (Standalone)")
     conan_install "$(get_build_dir Standalone)"
     ;;
@@ -88,20 +143,13 @@ case $taskName in
     conan_install "$(get_build_dir Library)"
     ;;
 
-# configure
-
-
 "Configure (Standalone)")
     cmake_configure "./Standalone" "$(get_build_dir Standalone)"
     ;;
 
-
 "Configure (Library)")
     cmake_configure "." "$(get_build_dir Library)"
     ;;
-
-
-
 
 "Build (Standalone)")
     cmake_configure "./Standalone" "$(get_build_dir Standalone)"
@@ -111,8 +159,6 @@ case $taskName in
     cmake_configure "." "$(get_build_dir Library)"
     cmake_build "$(get_build_dir Library)"
     ;;
-
-
 
 "Clean (Standalone)")
     cmake_clean "$(get_build_dir Standalone)"
