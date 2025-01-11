bb16fcd026e2231e5e36cbdb561f72f44952ff43
tomasmark79 <tomas@digitalspace.name>
2025-01-11 11:19:09 +0100

    v. upgrade

--- conanfile.py
+++ conanfile.py
@@ -5,15 +5,15 @@ class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
-
     def requirements(self):
         self.requires("zlib/1.2.11")
         self.requires("fmt/11.0.2")
         self.requires("nlohmann_json/3.11.2")
         #self.requires("libcurl/8.10.1")
     
-    #def build_requirements(self):
-        #self.tool_requires("cmake/3.22.6")
+    # def build_requirements(self):
+        # self.tool_requires("cmake/3.22.6")
 
-    #def layout(self):
-        #cmake_layout(self)
+    # WMVCMake is using own cmake_layout function, don't call it here
+    # def layout(self):
+        # cmake_layout(self)
