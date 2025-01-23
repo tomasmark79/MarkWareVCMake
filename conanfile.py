56a863c1b64358c2421b62cdd7bee6fa0b167a2b
tomasmark79 <tomas@digitalspace.name>
2025-01-23 16:37:00 +0100

    refactored conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,20 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout
 
-class CompressorRecipe(ConanFile):
+class MarkWareVCMake(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
 
     def requirements(self):
-        self.requires("zlib/1.2.11")
-        self.requires("fmt/11.1.1")
-        self.requires("nlohmann_json/3.11.2")
-        #self.requires("libcurl/8.10.1")
-    
-    # example
+        self.requires("zlib/1.2.11") 
+        self.requires("fmt/11.1.1") #required by cpm package
+        self.requires("nlohmann_json/3.11.2") # is modern to have json support
+        
     # def build_requirements(self):
         # self.tool_requires("cmake/3.22.6")
 
-    # !!! DO NOT USE Def Layout(self) with MARKWARE VCMAKE TEMPLATE !!!
-    # !!! MWVCMAKE IS USING OWN MULTICONFIG & MULTIPLATFORM LAYOUT  !!!
-    # !!! If you activate it, templae will goes broken              !!!
+    # MarkWareVCMake is using self mechanism to define the layout!
+    # Do not use defaull cmake_layout bellow or template goes broken!
     #def layout(self):
         # cmake_layout(self)
     
