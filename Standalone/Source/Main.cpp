b6994766b2fd6e058e573acf8349e0f3e1fcdfbf
tomasmark79 <tomas@digitalspace.name>
2025-02-08 11:43:46 +0100

    refactored to sanitizer scream

--- Standalone/Source/Main.cpp
+++ Standalone/Source/Main.cpp
@@ -1,23 +1,24 @@
 
 #include <VCMLib/VCMLib.hpp>
 #include <vcmlib/version.h>
-#include <chrono>
 #include <iostream>
 #include <memory>
-#include <thread>
 
-namespace Standalone
-{} // namespace Standalone
+// You may set sanitizer flags in CMakeLists.txt
+const bool TEST_SANITIZER = true;
 
-int main()
+namespace standalone
 {
-  // c++14+ way to create a unique pointer
-  std::unique_ptr<library::VCMLib> lib = std::make_unique<library::VCMLib>();
+  void saySomething()
+  {
+    std::cout << "Something!" << std::endl;
+  }
+}
 
-  // c++11 way to create a unique pointer
-  // std::unique_ptr<library::VCMLib> lib(new library::VCMLib());
-
-  // is-is-only-for-testing-purposes ------------👇🏻
+namespace sanitizer
+{
+  // You may set sanitizer flags in CMakeLists.txt
+  void scream()
   {
     char checkSanitizer[2];
     checkSanitizer[3] = 'a';
@@ -25,14 +26,18 @@ int main()
     delete[] checkSanitizerArr;
     checkSanitizerArr[0] = 0;
   }
-  // is-is-only-for-testing-purposes ------------👆🏻
-  // is-is-only-for-testing-purposes ------------👇🏻
-  {
-    std::cout << "Wait for 5 seconds please ..." << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    std::cout << "Bye bye!" << std::endl;
-  }
-  // is-is-only-for-testing-purposes ------------👆🏻
+}
+
+int main()
+{
+  // c++14+
+  // std::unique_ptr<library::VCMLib> lib = std::make_unique<library::VCMLib>();
+
+  // c++11
+  std::unique_ptr<library::VCMLib> lib(new library::VCMLib());
+
+  sanitizer::scream();
+  standalone::saySomething();
 
   return 0;
 }
