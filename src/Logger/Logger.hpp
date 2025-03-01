b9f30c038b492d59485fcafd4bf5199cac89c224
tomasmark79 <tomas@digitalspace.name>
2025-03-01 15:31:23 +0100

    improved logger and option parameters in main entry

--- src/Logger/Logger.hpp
+++ src/Logger/Logger.hpp
@@ -2,6 +2,7 @@
 #define LOGGER_H
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <chrono>
 #include <iomanip>
@@ -14,7 +15,13 @@
 
 class Logger {
 public:
-  enum class Level { LOG_DEBUG, LOG_INFO, LOG_WARNING, LOG_ERROR };
+  enum class Level {
+    LOG_DEBUG,
+    LOG_INFO,
+    LOG_WARNING,
+    LOG_ERROR,
+    LOG_CRITICAL
+  };
 
   // C++11 Singleton
   static Logger& getInstance() {
@@ -32,7 +39,7 @@ public:
     auto now_time = std::chrono::system_clock::to_time_t(now);
     std::tm now_tm = *std::localtime(&now_time);
 
-    std::cout << "[" << std::put_time(&now_tm, "%Y-%m-%d %H:%M:%S") << "] "
+    std::cout << "[" << std::put_time(&now_tm, "%d-%m-%Y %H:%M:%S") << "] "
               << "[" << name << "] "
               << "[" << levelToString(level) << "] ";
 
@@ -43,25 +50,54 @@ public:
     std::cout << std::endl;
   }
 
+  // Stream support for different log levels
+  Logger& operator<<(Level level) {
+    currentLevel = level;
+    return *this;
+  }
+
+  template<typename T>
+  Logger& operator<<(const T& message) {
+    std::lock_guard<std::mutex> lock(logMutex);
+
+    if (!messageStream.str().empty()) { messageStream << " "; }
+    messageStream << message;
+    return *this;
+  }
+
+  Logger& operator<<(std::ostream& (*os)(std::ostream&)) {
+    log(currentLevel, messageStream.str());
+    messageStream.str("");  // Clear the stream
+    messageStream.clear();  // Clear any error flags
+    return *this;
+  }
+
   void debug(const std::string& message) { log(Level::LOG_DEBUG, message); }
   void info(const std::string& message) { log(Level::LOG_INFO, message); }
   void warning(const std::string& message) { log(Level::LOG_WARNING, message); }
   void error(const std::string& message) { log(Level::LOG_ERROR, message); }
+  void critical(const std::string& message) {
+    log(Level::LOG_CRITICAL, message);
+  }
 
 private:
   std::string name =
       "VCMLib";  // string name may be affected by SolutionRenamer.py
   std::mutex logMutex;
 
+  Level currentLevel = Level::LOG_INFO;  // Default log level
+  std::ostringstream messageStream;
+
   Logger() = default;
   ~Logger() = default;
 
   std::string levelToString(Level level) const {
     switch (level) {
-      case Level::LOG_DEBUG: return "DEBUG";
-      case Level::LOG_INFO: return "INFO";
-      case Level::LOG_WARNING: return "WARNING";
-      case Level::LOG_ERROR: return "ERROR";
+      case Level::LOG_DEBUG: return "DEB";
+      case Level::LOG_INFO: return "INF";
+      case Level::LOG_WARNING: return "WAR";
+      case Level::LOG_ERROR: return "ERR";
+      case Level::LOG_CRITICAL: return "CRI";
       default: return "UNKNOWN";
     }
   }
@@ -86,6 +122,10 @@ private:
         SetConsoleTextAttribute(
             hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY);
         break;
+      case Level::LOG_CRITICAL:
+        SetConsoleTextAttribute(
+            hConsole, FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY);
+        break;
       default:
         SetConsoleTextAttribute(
             hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
@@ -104,6 +144,9 @@ private:
         break;
       case Level::LOG_ERROR:
         std::cout << "\033[31m";  // Red
+        break;
+      case Level::LOG_CRITICAL:
+        std::cout << "\033[95m";  // Lila
         break;
       default:
         std::cout << "\033[0m";  // Reset
@@ -125,9 +168,10 @@ private:
 // Define LOG macro for easy access
 #define LOG Logger::getInstance()
 
-// implementation examples
+// call logger examples
 // Logger::getInstance().log(Logger::Level::INFO, something);
 // Logger::getInstance().info(something);
 // LOG.info(something);
+// LOG << Logger::Level::LOG_INFO << "Loading library ommited" << std::endl;
 
 #endif  // LOGGER_H
--- standalone/src/Main.cpp
+++ standalone/src/Main.cpp
@@ -2,10 +2,13 @@
 #include <Logger/Logger.hpp>
 #include <cxxopts.hpp>
 #include "SanitizerTest.hpp"
+#include <iostream>
 #include <memory>
 
 // MIT License
 // Copyright (c) 2024-2025 Tomáš Mark
+
+constexpr char standaloneName[] = "VCMStandalone";
 
 #ifdef NDEBUG
 constexpr char build_type[] = "Release";
@@ -14,17 +17,15 @@ constexpr char build_type[] = "Debug";
 #endif
 
 int main(int argc, const char* argv[]) {
-  char hw[] = "Welcome in MarkWare VCMake!";
+  { LOG.info(standaloneName); }
   { LOG.info("C++ " + std::to_string(__cplusplus)); }
   { LOG.debug(build_type); }
-  { LOG.warning(hw); }
-  { LOG.error("No Error"); }
-  { LOG.critical("No Critical"); }
 
   try {
     std::unique_ptr<cxxopts::Options> allocated(
-        new cxxopts::Options(argv[0], " - example command line options"));
+        new cxxopts::Options(argv[0], standaloneName));
     auto& options = *allocated;
+
     options.positional_help("[optional args]").show_positional_help();
 
     // clang-format off
@@ -34,36 +35,35 @@ int main(int argc, const char* argv[]) {
       .allow_unrecognised_options()
       .add_options()
       ("h,help", "Show help")
-      ("o,ommit", "Ommit library", cxxopts::value<bool>()->default_value("false"))
-      ("s,sanitizer", "Sanitizer test", cxxopts::value<bool>()->default_value("false"));
+      ("o,ommit", "Ommit library loading", cxxopts::value<bool>()->default_value("false"))
+      ("s,sanitizer", "Test sanitizer", cxxopts::value<bool>()->default_value("false"));
 
     // clang-format on
 
     auto result = options.parse(argc, argv);
 
     if (result.count("help")) {
-      std::cout << options.help({"", "Group"}) << std::endl;
+      LOG << Logger::Level::LOG_INFO << options.help({"", "Group"})
+          << std::endl;
       return 0;
     }
 
     if (!result.count("ommit")) {
-      std::cout << "Ommit library loading" << std::endl;
       std::unique_ptr<library::VCMLib> lib(new library::VCMLib());
+    } else {
+      LOG << Logger::Level::LOG_WARNING << "Loading library ommited [-o]"
+          << std::endl;
     }
 
     if (result.count("sanitizer")) {
+      LOG << Logger::Level::LOG_WARNING << "Testing sanitizer" << std::endl;
       sanitizerTest::scream();
     }
 
   } catch (const cxxopts::exceptions::exception& e) {
-    std::cout << "error parsing options: " << e.what() << std::endl;
+    LOG << Logger::Level::LOG_ERROR << "error parsing options: " << e.what();
     return 1;
   }
-
-  // try {
-  //   std::unique_ptr<library::VCMLib> lib(new library::VCMLib());
-  //   sanitizerTest::scream();
-  // } catch (std::exception& e) { LOG.error(e.what()); }
 
   return 0;
 }
