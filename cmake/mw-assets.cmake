71faad74f9a3cad2c6d4b619d123c4e4c39ca0db
tomasmark79 <admin@digitalspace.name>
2025-03-07 11:34:46 +0100

    Asset processing, Linux done, Windows Todo

---
+++ cmake/mw-assets.cmake
@@ -0,0 +1,56 @@
+# MIT License
+# Copyright (c) 2024-2025 Tomáš Mark
+
+function(copy_assets target asset_sources destination)
+    add_custom_command(
+        TARGET ${target}
+        POST_BUILD
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${destination}"
+        COMMAND ${CMAKE_COMMAND} -E copy_directory ${asset_sources} "${destination}")
+endfunction()
+
+function(apply_assets_processing)
+    set(ASSET_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/../assets/")
+    set(ASSET_INSTALL_DIR_POSIX "share/${STANDALONE_NAME}/assets")
+    set(ASSET_INSTALL_DIR_WIN32 "assets")
+    set(ASSET_BUILD_DIR_POSIX "$<TARGET_FILE_DIR:${STANDALONE_NAME}>/../${ASSET_INSTALL_DIR_POSIX}")
+    set(ASSET_BUILD_DIR_WIN32 "$<TARGET_FILE_DIR:${STANDALONE_NAME}>/${ASSET_INSTALL_DIR_WIN32}")
+
+    if(UNIX AND NOT APPLE)
+        set(ASSET_BUILD_DIR ${ASSET_BUILD_DIR_POSIX})
+        set(ASSET_INSTALL_DIR ${ASSET_INSTALL_DIR_POSIX})
+    elseif(APPLE)
+        set(ASSET_BUILD_DIR ${ASSET_BUILD_DIR_POSIX})
+        set(ASSET_INSTALL_DIR ${ASSET_INSTALL_DIR_POSIX})
+    elseif(WIN32)
+        set(ASSET_BUILD_DIR ${ASSET_BUILD_DIR_WIN32})
+        set(ASSET_INSTALL_DIR ${ASSET_INSTALL_DIR_WIN32})
+    endif()
+
+    file(GLOB_RECURSE ASSET_FILES "${ASSET_SOURCE_DIR}/*")
+    if(NOT ASSET_FILES)
+        message(STATUS "No asset files found in ${ASSET_SOURCE_DIR}.")
+        target_compile_definitions(${STANDALONE_NAME} PRIVATE ASSET_FILES="No asset files found in ${ASSET_SOURCE_DIR}.")
+        return()
+    endif()
+    
+    # Extract file names from paths
+    set(ASSET_FILE_NAMES "")
+    foreach(ASSET_FILE ${ASSET_FILES})
+        get_filename_component(FILE_NAME ${ASSET_FILE} NAME)
+        list(APPEND ASSET_FILE_NAMES ${FILE_NAME})
+    endforeach()
+    
+    string(REPLACE ";" "," ASSET_FILE_NAMES_STR "${ASSET_FILE_NAMES}")
+    
+    # Define compile-time #define variables
+    target_compile_definitions(${STANDALONE_NAME} PRIVATE ASSET_PATH="${ASSET_INSTALL_DIR}")
+    target_compile_definitions(${STANDALONE_NAME} PRIVATE ASSET_FILES="${ASSET_FILE_NAMES_STR}")
+    
+    # Copy to build directory
+    copy_assets(${STANDALONE_NAME} "${ASSET_SOURCE_DIR}" "${ASSET_BUILD_DIR}")
+    
+    # Install
+    install(DIRECTORY ${ASSET_SOURCE_DIR} DESTINATION ${ASSET_INSTALL_DIR})
+
+endfunction()
--- standalone/src/Main.cpp
+++ standalone/src/Main.cpp
@@ -10,34 +10,10 @@
 
 constexpr char standaloneName[] = "VCMStandalone";
 
-// ==============================================================================
-//
-// ==============================================================================
-#ifdef NDEBUG
-constexpr char build_type[] = "Release";
-#else
-constexpr char build_type[] = "Debug";
-#endif
-
-#ifdef _WIN32
-#define ASSETS_PATH "../assets/IAmAsset.txt"
-#elif defined(__unix__) || defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__)
-#define ASSETS_PATH "../share/VCMStandalone/assets/IAmAsset.txt"
-#else
-#error "Set your platform path for assets folder"
-#endif
-
-int main(int argc, const char* argv[]) {
-  LOG.info(standaloneName);
-  LOG.info("C++ " + std::to_string(__cplusplus));
-  LOG.debug(build_type);
-
+/// @brief process command line arguments
+int parseTemplateOptions(int argc, const char* argv[]) {
   try {
     // clang-format off
-
-    // ==============================================================================
-    // Initialize options
-    // ==============================================================================
     auto options = std::make_unique<cxxopts::Options>(argv[0], standaloneName);
     options->positional_help("[optional args]").show_positional_help();
     options->set_width(70)
@@ -45,48 +21,36 @@ int main(int argc, const char* argv[]) {
       .allow_unrecognised_options()
       .add_options()
       ("h,help", "Show help")
-      ("a,assets", "Omit assets loading", cxxopts::value<bool>()->default_value("false"))
       ("o,omit", "Omit library loading", cxxopts::value<bool>()->default_value("false"));
     // clang-format on
-
     auto result = options->parse(argc, argv);
-  
-    // ==============================================================================
-    // Show help
-    // ==============================================================================
     if (result.count("help")) {
       LOG << Logger::Level::LOG_INFO << options->help({"", "Group"})
           << std::endl;
       return 0;
     }
-
-    // ==============================================================================
-    // Show assets path
-    // ==============================================================================
-    if (result.count("assets")) {
-      LOG << Logger::Level::LOG_WARNING << "Loading assets omitted [-a]"
-          << std::endl;
-    } else {
-      LOG << Logger::Level::LOG_INFO
-          << "Asset file path: " << ASSETS_PATH << std::endl;
-          // Load assets
-          // ...
-    }
-
-    // ==============================================================================
-    // Load library
-    // ==============================================================================
     if (!result.count("omit")) {
-      std::unique_ptr<library::VCMLib> lib(new library::VCMLib());
+      std::unique_ptr<library::VCMLib> lib =
+          std::make_unique<library::VCMLib>();
     } else {
       LOG << Logger::Level::LOG_WARNING << "Loading library omitted [-o]"
           << std::endl;
     }
-
   } catch (const cxxopts::exceptions::exception& e) {
     LOG << Logger::Level::LOG_ERROR << "error parsing options: " << e.what();
     return 1;
   }
-
   return 0;
+}
+
+/// @brief Main Standalone entry point
+int main(int argc, const char* argv[]) {
+  LOG.info(standaloneName);
+  LOG.info("C++ " + std::to_string(__cplusplus));
+  LOG << Logger::Level::LOG_INFO << static_cast<std::string>(ASSET_PATH)
+      << std::endl;
+  LOG << Logger::Level::LOG_INFO << static_cast<std::string>(ASSET_FILES)
+      << std::endl;
+
+  return parseTemplateOptions(argc, argv);
 }
